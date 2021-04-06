029ad01713e2655eb3123e6743a615f6a6fd7c21
speraza545 <s.peraza545@gmail.com>
2021-04-06 18:50:29 -0400

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,10 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy
+    attr_accessor :name, :age, :breed
+
+    def initialize(hash)
+        @name = hash[:name]
+        @breed = hash[:breed]
+        @age = hash[:age]
+    end
+end
